caafb2cf70b2571f4995e4d194c70b23ef190995
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-04-26 12:26:31 -0400

    Added a bool that checks if the timer shows or not, and a ZStack with two icons to toggle said bool.

--- ComposableViewsAndAnimations/Views/Assignment/AssignmentAnimationView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/AssignmentAnimationView.swift
@@ -9,25 +9,32 @@ import SwiftUI
 
 struct AssignmentAnimationView: View {
     let totalTime: Double
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State var timePassed = 0.0
+    @State var isTimerRunning = false
     var body: some View {
-        ZStack {
-            Circle()
-                .trim(from: 0, to: (totalTime - timePassed) / totalTime)
-                .stroke(Color.red, lineWidth: 15)
-                .frame(width: 200, height: 200)
-                .rotationEffect(.degrees(-90))
-            Text("\(Int(totalTime - timePassed))")
-                .font(.title)
-        }
-        .onTapGesture {
-            withAnimation(.linear(duration: totalTime)) {
-                timePassed = totalTime
+        VStack(spacing: 20) {
+            ZStack {
+                Image(systemName: "pause.circle")
+                    .opacity(isTimerRunning ? 1.0 : 0.0)
+                Image(systemName: "play.circle")
+                    .opacity(isTimerRunning ? 0.0 : 1.0)
+            }
+            .onTapGesture {
+                isTimerRunning != isTimerRunning
+            }
+            ZStack {
+                Circle()
+                    .trim(from: 0, to: (totalTime - timePassed) / totalTime)
+                    .stroke(Color.red, lineWidth: 15)
+                    .frame(width: 200, height: 200)
+                    .rotationEffect(.degrees(-90))
+                Text("\(totalTime - timePassed)")
+                    .font(.title)
             }
         }
     }
 }
-
 struct AssignmentAnimationView_Previews: PreviewProvider {
     static var previews: some View {
         AssignmentAnimationView(totalTime: 100.0)
