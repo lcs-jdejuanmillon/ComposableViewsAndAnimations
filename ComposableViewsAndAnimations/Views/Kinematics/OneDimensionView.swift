de13a4afe642aabd5743cef9fce0ce36da6c4212
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-04-29 23:38:37 -0400

    Made the view accept input, and introduced a scale factor to make the movement visible in all cases (need to introduce scale for reference)

--- ComposableViewsAndAnimations/Views/Kinematics/OneDimensionView.swift
+++ ComposableViewsAndAnimations/Views/Kinematics/OneDimensionView.swift
@@ -4,18 +4,22 @@
 //
 //  Created by Jacobo de Juan Millon on 2022-04-29.
 //
- 
+
 import SwiftUI
- 
+
 struct OneDimensionView: View {
     let timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
-    let initialVelocity = 10.0
-    let acceleration = 1.0
-    let totalTime = 10.0
+    let initialVelocity: Double
+    let acceleration: Double
+    let totalTime: Double
+    var scaleFactor: Double {
+        return 400 / totalTime / (initialVelocity + totalTime * acceleration)
+    }
     @State var time = 0.0
     var body: some View {
         VStack {
             HStack {
+                Spacer()
                 ComposableViewsAndAnimations(size: 50.0,
                                              width: 0.2,
                                              fontSize: 0.5,
@@ -26,23 +30,26 @@ struct OneDimensionView: View {
                                              runAutomatically: true)
                 
             }
-        Circle()
-            .frame(width: 50, height: 50)
-            .offset(x: 0, y: time * (initialVelocity + time * acceleration / 2))
-            .onReceive(timer) { input in
-                withAnimation(.linear(duration: 10.0)) {
-                    time = totalTime
+            Circle()
+                .frame(width: 50, height: 50)
+                .offset(x: 0, y: scaleFactor * time * (initialVelocity + time * acceleration / 2))
+                .onReceive(timer) { input in
+                    withAnimation(.linear(duration: 10.0)) {
+                        time = totalTime
+                    }
+                    timer.upstream.connect().cancel()
                 }
-                timer.upstream.connect().cancel()
-            }
-        Spacer()
+            Spacer()
         }
+        .padding()
     }
 }
- 
+
 struct OneDimensionView_Previews: PreviewProvider {
     static var previews: some View {
-        OneDimensionView()
+        OneDimensionView(initialVelocity: 4.0,
+                         acceleration: 2.5,
+                         totalTime: 10.0)
     }
 }
 
