c8eb6145003c6939b3d6a755da83c9eb2196fbcc
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-05-01 17:54:08 -0400

    Implemented Kinematics Landing View. Kept it simple.

--- ComposableViewsAndAnimations/Views/Kinematics/KinematicsChoosingView.swift
+++ ComposableViewsAndAnimations/Views/Kinematics/KinematicsChoosingView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct KinematicsChoosingView: View {
+    @State private var inputTime: String = ""
+    @State private var inputInitialVelocityX: String = ""
+    @State private var inputInitialVelocityY: String = ""
+    @State private var inputAccelerationX: String = ""
+    @State private var inputAccelerationY: String = ""
+    var time: Double {
+        if let time = Double(inputTime) {
+            if time > 0.05 {
+                return time
+            }
+        }
+        return -1.0
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            
+            Group {
+                TextField("Time in Seconds", text: $inputTime)
+                    .foregroundColor(time > 0 ? .red : .primary)
+                TextField("Initial Horizontal Velocity in m/s", text: $inputInitialVelocityX)
+                    .foregroundColor(validInput(input: inputInitialVelocityX) ? .red : .primary)
+                TextField("Initial Vertical Velocity in m/s", text: $inputInitialVelocityY)
+                    .foregroundColor(validInput(input: inputInitialVelocityY) ? .red : .primary)
+                TextField("Horizontal Acceleration in m/s2", text: $inputAccelerationX)
+                    .foregroundColor(validInput(input: inputAccelerationX) ? .red : .primary)
+                TextField("Vertical Acceleration in m/s2", text: $inputAccelerationY)
+                    .foregroundColor(validInput(input: inputAccelerationY) ? .red : .primary)
+            }
+            .padding(.bottom)
+            
+            List {
+                NavigationLink(destination: ProjectileView(initialVelocity: [4.0, 1.0],
+                                                           acceleration: [2.0, -10.0],
+                                                           totalTime: Double(inputTime)!)) {
+                    SimpleListItemView(title: "Kinematics Animation",
+                                       caption: "Movement of an object in two dimensions with constant acceleration")
+                }
+            }
+            .opacity(time > 0 && validInput(input: inputInitialVelocityX) && validInput(input: inputInitialVelocityX) && validInput(input: inputAccelerationX) && validInput(input: inputAccelerationY) ? 0.0 : 1.0)
+            
+        }
+        .padding()
+        .navigationTitle("My Composable View")
+    }
+    func validInput(input: String) -> Bool {
+        if let _ = Double(input) {
+            return true
+        }
+        return false
     }
 }
 
